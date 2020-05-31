53af8042b59ecf83381e125f852b9e4c0d77daa8
ulvimacid <ulvizm@code.edu.az>
2020-05-31 13:44:03 +0400

    first commit

---
+++ Files/CustomerControlApplication/Customer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerControlApplication
+{
+    class Customer
+    {
+        #region Fieldler
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerLastName { get; set; }
+        public string CustomerEmail { get; set; }
+        public int Password { get; set; }
+        #endregion
+        #region Encapsulation for Kullanici adi
+        private string _customerKullaniciAdi { get; set; }
+        public string KullaniciAdi
+        {
+            get
+            {
+                return _customerKullaniciAdi;
+            }
+            set
+            {
+                bool KullaniciAdi = CustomerNameControl(value);
+                if (KullaniciAdi)
+                {
+                    Console.WriteLine("Bu istifadeci adi artiq movcuddur.");
+                    this._customerKullaniciAdi = string.Empty;
+
+                }
+                else
+                {
+                    _customerKullaniciAdi = value;
+                }
+            }
+        }
+        #endregion
+        #region static memmbers
+        static ArrayList CustomerArray;
+        static Customer()
+        {
+            CustomerArray = new ArrayList();
+        }
+        static bool CustomerNameControl (string _customerKullaniciAdi)
+        {
+            bool control = false;
+            for (int i = 0; i < CustomerArray.Count; i++)
+            {
+                Customer musteri = (Customer)CustomerArray[i];
+            if (musteri.KullaniciAdi==_customerKullaniciAdi)
+            {
+                    control = true;
+                    break;
+            }          
+
+            }
+            return control;
+        }
+        public static void CustomerAdd(Customer customer)
+        {
+            if (customer!=null&& !string.IsNullOrEmpty(customer.KullaniciAdi)&& !string.IsNullOrEmpty(customer.CustomerEmail))
+            {
+                bool customerEmailcontrol = CustomerEmailControl(customer.CustomerEmail);
+                if (customerEmailcontrol)
+                {
+                    Console.WriteLine("Bu email artiq movcuddur");
+                }
+                else
+                {
+                    CustomerArray.Add(customer);
+                    Console.WriteLine("Adding succsesfully");
+                }
+            }
+        }
+        static bool CustomerEmailControl(string _email)
+        {
+            bool control = false;
+            for (int i = 0; i < CustomerArray.Count; i++)
+            {
+                Customer musteri = (Customer)CustomerArray[i];
+                if (musteri.CustomerEmail==_email)
+                {
+                     control = true;
+                break;
+                }
+            }
+            return control;
+        }
+        #endregion
+    }
+}
---
+++ Files/CustomerControlApplication/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerControlApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Adding objects
+            Customer customer = new Customer();
+            customer.CustomerId = 1;
+            customer.CustomerName = "Ulvi";
+            customer.CustomerLastName = "Mecidov";
+            customer.CustomerEmail = "UlviMacid@mail.ru";
+            customer.KullaniciAdi = "UlviMacid";
+            customer.Password = 123;
+            Customer.CustomerAdd(customer);
+
+            Customer customer1 = new Customer();
+            customer1.CustomerId = 2;
+            customer1.CustomerName = "Eli";
+            customer1.CustomerLastName = "Aliyev";
+            customer1.CustomerEmail = "AliEliyev@mail.ru";
+            customer1.KullaniciAdi = "AliAli";
+            customer1.Password = 123123;
+            Customer.CustomerAdd(customer1);
+
+            Customer customer2 = new Customer();
+            customer2.CustomerId = 2;
+            customer2.CustomerName = "Orxan";
+            customer2.CustomerLastName = "Mecidov";
+            customer2.CustomerEmail = "UlviMacid@mail.ru";
+            customer2.KullaniciAdi = "UlviMacid1";
+            customer2.Password = 123;
+            Customer.CustomerAdd(customer2);
+            #endregion
+        }
+    }
+}
